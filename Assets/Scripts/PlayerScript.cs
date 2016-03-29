279a2ec5f7934872fbbb7edc391b06709deda052
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-03-30 07:11:25 +0800

    Plane, Player, Wall, Control, Camera

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate() {
+		if (Input.GetKey (KeyCode.Space)) {
+			rb.velocity = rb.velocity * 0.95f;
+		} else {
+			rb.AddForce (new Vector3 (
+				Input.GetAxis ("Horizontal"),
+				0,
+				Input.GetAxis ("Vertical")
+			) * speed);
+		}
+	}
+}
