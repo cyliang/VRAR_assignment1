67d587ae851ba85137158ea5c8c04b99be7e4c7c
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-03-31 00:46:15 +0800

    Info Boards & Treasures

---
+++ Assets/Scripts/TreasureScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreasureScript : MonoBehaviour {
+
+	public float speed;
+	public float distance;
+
+	Vector3 position;
+
+	void Start () {
+		position = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = position + Vector3.up * Mathf.Sin (speed * Time.time) * distance;
+	}
+}
